494fdeb97e7de97fa7f48c663f996cd1bbb6fc35
jshastid <jamesgshastid@gmail.com>
2025-09-05 20:28:25 -0400

    added some inputs for the add modal screen

--- src/choresComponents/choreModal.js
+++ src/choresComponents/choreModal.js
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions } from "react-native";
 import Animated, { SlideInDown, SlideOutDown, FadeIn, FadeOut } from "react-native-reanimated"
 
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export function ChoreModal({hideModal}) {
   const dim = Dimensions.get("screen");
@@ -30,6 +31,27 @@ export function ChoreModal({hideModal}) {
         exiting={SlideOutDown}
       >
 
+      <SafeAreaView style={styles.primaryBody}>
+
+        <Text style={styles.inputLabel}>
+          Chore Name
+        </Text>
+        <TextInput 
+          placeholder="wash clothes, mow lawn, make bed..."
+          style={styles.inputShape}
+        />
+
+        <Text style={styles.inputLabel}>
+          Chore Description
+        </Text>
+        <TextInput 
+          placeholder="Add details about the chore and any notes to help you remember"
+          style={styles.inputShape}
+        />
+
+      </SafeAreaView>
+
+
       </Animated.View>
     </>
   );
@@ -45,8 +67,25 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 0,
     width: "100%",
-    height: "80%",
+    height: "100%",
     backgroundColor: "white",
-    borderRadius: 35,
+    borderRadius: 12,
   },
+  inputLabel: {
+    fontWeight: "bold",
+    marginLeft: "5%",
+    marginBottom: 2,
+    fontSize: 16,
+  },
+  primaryBody: {
+    padding: "5%",
+  },
+  inputShape: {
+    borderWidth: 1,
+    borderRadius: 14,
+    width: "90%",
+    margin: "auto",
+    backgroundColor: "#f7f7f7ff",
+    borderColor: "#ccccccff"
+  }
 });
